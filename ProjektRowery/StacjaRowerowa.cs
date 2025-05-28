052ffbf267d67ea03afdc9b5026b1c5d68d6b445
Bartosz Śnaidkowski <bsniadkowski2004@gmail.com>
2025-05-28 22:49:30 +0200

    StacjaRowerowa

---
+++ ProjektRowery/StacjaRowerowa.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjektRowery
+{
+    class StacjaRowerowa
+    {
+        public List<string> ListaRowerow { get; private set; }
+
+        public int LiczbaMiejsc { get; private set; }
+
+        public string NazwaStacji { get; private set; }
+
+        public int LiczbaWolnychMiejsc => LiczbaMiejsc - ListaRowerow.Count;
+        
+
+
+
+        public StacjaRowerowa(int liczbaMiejsc, string nazwaStacji, List<string> poczatkoweRowery = null)
+        {
+            LiczbaMiejsc = liczbaMiejsc;
+            NazwaStacji = nazwaStacji;
+            ListaRowerow = poczatkoweRowery ?? new List<string>();
+        }
+
+        public bool CzyMoznaOddacRower()
+        {
+            return LiczbaWolnychMiejsc > 0;
+        }
+
+        public  bool czyMoznaWypozyczycRower(string nazwa)
+        {
+            return ListaRowerow.Contains(nazwa);
+        }
+
+    }
+}
